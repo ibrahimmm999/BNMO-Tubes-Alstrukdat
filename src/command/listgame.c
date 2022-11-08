e205530123d3525060c20e0555b4ce4a58c327aa
taraharyono <103413692+taraharyono@users.noreply.github.com>
2022-11-08 23:18:07 +0700

    edit jadi pake TulisIsi

--- src/command/listgame.c
+++ src/command/listgame.c
@@ -4,7 +4,5 @@
 void LISTGAME(Arr arrgame){
     printf("Berikut adalah daftar game yang tersedia\n");
 
-    for (int i=0; i<arrgame.Neff; i++){
-        printf("%d. %s\n", i+1, arrgame.A[i]);
-    }
+    TulisIsi(arrgame);
 }

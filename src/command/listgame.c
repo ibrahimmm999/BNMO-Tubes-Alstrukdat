48a86a4a967f30ffd4aab74dee8571fac33c3c28
taraharyono <103413692+taraharyono@users.noreply.github.com>
2022-11-08 23:03:02 +0700

    edit nama fungsi

--- src/command/listgame.c
+++ src/command/listgame.c
@@ -1,7 +1,7 @@
 #include "listgame.h"
 #include <stdio.h>
 
-void listgame(Arr arrgame){
+void LISTGAME(Arr arrgame){
     printf("Berikut adalah daftar game yang tersedia\n");
 
     for (int i=0; i<arrgame.Neff; i++){

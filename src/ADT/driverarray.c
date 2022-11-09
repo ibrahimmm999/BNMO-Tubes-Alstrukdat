1e3433fa04debeb4e5592a9c03b33ec7cf556dc8
gevyndo <gevyndo18@gmail.com>
2022-11-09 23:21:17 +0700

    Update driverarray.c

--- src/ADT/driverarray.c
+++ src/ADT/driverarray.c
@@ -7,7 +7,28 @@ int main()
     MakeEmpty(&bocol);
     //buat array kosong
 
+    int NbElmt(Arr T);
+    Word gunting;
 
+    gunting.TabWord[0]='g';
+    gunting.TabWord[1]='u';
+    gunting.TabWord[2]='n';
+    gunting.TabWord[3]='t';
+    gunting.TabWord[4]='i';
+    gunting.TabWord[5]='n';
+    gunting.TabWord[6]='g'; 
+    gunting.Length=7;
+
+    SetEl(&bocol, 0, gunting);
+
+    NbElmt(bocol);
+    //nilai element bocol
+    MaxNbEl(bocol);
+    // nilai element maksimal
+
+    GetFirstIdx(bocol);
+
+    GetLastIdx(bocol);
     return 0;
 }
 

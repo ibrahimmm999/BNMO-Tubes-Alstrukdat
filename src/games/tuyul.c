aaa9197ce7e8970338fed1bc373ee7f968e2ac6f
gevyndo <gevyndo18@gmail.com>
2022-11-09 23:01:42 +0700

    GUNTING BATU KERTAS (easy mode)

---
+++ src/games/tuyul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../ADT/mesinkata.h"
+#include "../ADT/array.h"
+#include "../boolean.h"
+#include <time.h>
+
+void gbk()
+{
+   
+}    
+
---
+++ src/games/tuyul.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../ADT/mesinkata.h"
+#include "../ADT/list.h"
+#include "../boolean.h"
+
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
+
+void gbk();
+
+#endif

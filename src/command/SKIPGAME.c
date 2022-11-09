570de16ad921bafa36e34bf39a14a902d4cd2d9a
Yasmin Arum Sari <88687701+yasminarsa@users.noreply.github.com>
2022-11-09 15:00:09 +0700

    Update and rename skip_game.c to SKIPGAME.c

---
+++ src/command/SKIPGAME.c
@@ -0,0 +1,35 @@
+#include "SKIPGAME.h"
+
+void SKIPGAME(Queue *queueGame, int numSkip){
+    /* Kamus */
+    int i = 0;
+    boolean notEmpty = false;
+    ElType val;
+    Word currentGame = HEAD(*queueGame);
+    /* Algoritma */
+    printf("Berikut adalah daftar Game-mu\n"); // Menampilkan antrean Game pemain
+    displayQueue(*queueGame); // Menampilkan Game-Game dalam antrean pemain secara vertikal
+    if(numSkip > 0){ // Percabangan untuk memastikan parameter input numSkip valid (bukan nol dan bukan pula bilangan negatif)
+        while(i < numSkip && notEmpty == false){ // Pengulangan untuk 'menghapus' numSkip buah Game pada antrean awal
+            dequeue(queueGame, &val); // 'Menghapus' Game pada antrean pertama
+            if(length(*queueGame) == 0){ // Percabangan apabila sudah tidak ada lagi Game dalam antrean
+                notEmpty = true; // Assign true 
+            }
+        }    
+    }
+    if(length(*queueGame) == 0){ // Percabangan apabila tidak ada Game untuk dimainkan dalam antrean Game pemain
+        printf("Tidak ada permainan lagi dalam daftar Game-mu\n");
+    } else { //length(*queueGame) != 0 -> memulai permainan pada antrean teratas
+        printf("Loading");
+        displayCurrentGame(currentGame);
+        printf(" ...\n");
+        if (IsWordEqual(currentGame, "RNG")){
+            RNG();
+        } else if (IsWordEqual(currentGame, "Diner DASH")){
+            printf("diner dash");
+        } else {
+            printf("Game %s masih dalam maintenance, belum dapat dimainkan. Silakan pilih game lain.", wordToString(currentGame, false));
+        }
+        dequeue(queueGame, &currentGame);
+    }
+}

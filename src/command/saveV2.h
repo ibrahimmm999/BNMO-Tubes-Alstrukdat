9a0580cd238483a28fb598cc2ba1685b65019de2
a2ngel <99087033+a2ngel@users.noreply.github.com>
2022-11-27 14:25:02 +0700

    tambahan scoreboard buat header save

--- src/command/saveV2.h
+++ src/command/saveV2.h
@@ -1,9 +1,20 @@
 #include "../ADT/array.h"
 #include "../ADT/stackV2.h"
+#include "../ADT/mapV2.h"
 
 #ifndef SAVE_H
 #define SAVE_H
 
-void save(char *file_name, Arr list_game, StackW history);
+/*
+Prosedur untuk menyimpan state game (list game, history, scoreboard yang dimiliki sistem)
+I.S. game belum disimpan
+F.S. state game disimpan pada suatu file. Jika file sudah ada sebelumnya, make file akan dioverwrite
+*/
+void save(char *file_name, Arr list_game, StackW history, Map SBrng, Map SBdd, Map SBhm, Map SBtoh, Map SBsom);
+
+/*
+Prosedur simpan scoreboard ke dalam file
+*/
+void SaveSB(Map SBGame);
 
 #endif

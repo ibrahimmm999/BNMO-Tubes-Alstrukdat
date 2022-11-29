8c9b76c12b2f050609d6af8420110baab01574af
a2ngel <99087033+a2ngel@users.noreply.github.com>
2022-11-29 18:27:50 +0700

    Update resetscoreboard.h

--- src/command/resetscoreboard.h
+++ src/command/resetscoreboard.h
@@ -10,7 +10,7 @@ Prosedur Menampilkan daftar scoreboard dan meminta masukan berupa nomor
 scoreboard yang ingin di-reset.
 Reset dilakukan dengan memanggil prosedur lain (RSB) sesuai dengan nomor yang dimasukkan.
 */
-void reset_scoreboard(Map SBrng, Map SBdd, Map SBhm, Map SBtoh, Map SBsom);
+void reset_scoreboard(Map *SBrng, Map *SBdd, Map *SBhm, Map *SBtoh, Map *SBsom);
 
 /*
 Prosedur untuk menghapus (reset) semua informasi

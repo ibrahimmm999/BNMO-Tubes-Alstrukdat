38483001bbadfdc2b43cdaef0c241bea56a31fe0
a2ngel <99087033+a2ngel@users.noreply.github.com>
2022-12-02 09:47:16 +0700

    ubah jadi set

--- src/command/saveV2.h
+++ src/command/saveV2.h
@@ -10,7 +10,7 @@ Prosedur untuk menyimpan state game (list game, history, scoreboard yang dimilik
 I.S. game belum disimpan
 F.S. state game disimpan pada suatu file. Jika file sudah ada sebelumnya, make file akan dioverwrite
 */
-void save(char *file_name, Arr list_game, StackW history, MapV2 SBrng, MapV2 SBdd, MapV2 SBhm, MapV2 SBtoh, MapV2 SBsom);
+void save(char *file_name, Arr list_game, StackW history, Set scoreboard);
 
 /*
 Prosedur simpan scoreboard ke dalam file
